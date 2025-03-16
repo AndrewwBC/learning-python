42bd39a1021d3a76e25ee6f8200e8741958ddd46
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-15 23:27:53 -0300

    variables and types

---
+++ Chapter_2/types.py
@@ -0,0 +1,12 @@
+number: int = 4
+number: float = 3.14
+
+name: str = "Andrew"
+
+isTrue: bool = True
+
+listOfFruits: list = {"Apple", "Banana"}
+
+tupleOfFruits: tuple = {"Watermelon", "Orange"}
+
+isLikeMapInJava: dict = {"name": "Andrew", "name": "Joseph"}
---
+++ Chapter_2/variables.py
@@ -0,0 +1,14 @@
+"""
+    Python has dinamic typing, so I dont need
+    to type the variables
+    
+    Any variable declared outside a function is global
+    It's lifetime is the lifetime of the program
+    
+"""
+
+age = 28
+
+heigth = 1.80
+
+name = "Joseph"
