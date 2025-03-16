a4ed433040e8921d8f53c0a956616f60add1aa02
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-16 08:44:14 -0300

    printing

--- Chapter_2/types.py
+++ Chapter_2/types.py
@@ -1,4 +1,4 @@
-number: int = 4
+numberInt: int = 4
 number: float = 3.14
 
 name: str = "Andrew"
@@ -10,3 +10,9 @@ listOfFruits: list = {"Apple", "Banana"}
 tupleOfFruits: tuple = {"Watermelon", "Orange"}
 
 isLikeMapInJava: dict = {"name": "Andrew", "name": "Joseph"}
+
+floatToInt = int(number)
+intToFloat = float(numberInt)
+floatToString = str(number)
+
+print(f"FloatToInt: {floatToInt}, IntToFloat: {intToFloat}, floatToString: {floatToString}")
