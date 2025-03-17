b12c128887c2e68f73ed7ee5c43ee8f1a3282aaf
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-17 18:00:29 -0300

    dict

---
+++ Chapter_7/dict.py
@@ -0,0 +1,34 @@
+# Key-Value
+
+infos = {"name": "Andrew", "age": 25}
+
+nested = {
+    "id_0": {"name": "Campos", "age": 76},
+    "id_1": {"name": "Andrew", "age": 25},
+    "id_2": {"name": "Joseph", "age": 26},
+    "id_3": {"name": "Toto", "age": 27}
+}
+print(nested.values())
+# elements
+
+for data in range(len(nested)):
+    print(nested["id_"+str(data)])
+
+# dict methods
+
+# shallow copy 
+infos.copy()
+# get the value corresponding to the key
+infos.get("name")
+# returns all key/values 
+infos.items()
+# returns all keys
+infos.keys()
+# returns all values
+infos.values()
+# returns and delete the item corresponding to the key
+infos.pop("name")
+# returns and delete a random key/value
+infos.popitem()
+# remove all elements
+infos.clear()
