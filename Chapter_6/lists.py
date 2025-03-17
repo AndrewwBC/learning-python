f4e0a07727bd34110b380dea8a61c0553327dcba
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-16 21:44:07 -0300

    shallow and deep copy

--- Chapter_6/lists.py
+++ Chapter_6/lists.py
@@ -1,3 +1,5 @@
+import copy
+
 listOfFruits: list = ["Apple", "Banana", "Pineapple"]
 
 # get the list length
@@ -30,8 +32,30 @@ listTwo: list = [4,5,6]
 
 listThree = 1 * listOne + 4 * listTwo
 
-print(listThree)
+#print(listThree)
 
+# Copying lists
+# Assigning a list to another variable does not
+# create a new list. This means that both lists
+# will be changed when one is changed
+
+copyOfList = listThree
+#print(id(copyOfList), id(listThree))
+
+# Shallow Copy copies the memory address, 
+# but only the superficial elements
+# This means that it's possible to make
+# references to children nodes of the first node, thinking of a tree
+# but it will not appear in the copy, only in the original
+print(listThree.copy())
+print(listThree[:])
+
+# DeepCopy will not maintain the address memory
+# It will generate another object with all the
+# elements of the original object, but in a diferent
+# memory space
+
+print(copy.deepcopy(listOfFruits))
 
 
 
