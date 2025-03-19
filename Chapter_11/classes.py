1be384009e26d1c8114052a42d16745fe62d5222
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-19 14:53:53 -0300

    static, class, and instance methods

--- Chapter_11/classes.py
+++ Chapter_11/classes.py
@@ -1,5 +1,9 @@
 class User:
+    # Class atribute
+    specie = "Human"
+    
     def __init__(self, name, age):
+        # Instance atributes
         self.name = name
         self.age = age
     
@@ -10,3 +14,33 @@ class User:
 user = User("Andrew", 25)
 
 print(user.Hello())
+
+class Math:
+    a = 6
+    b = 2
+    
+    def __init__(self, c):
+        self.c = c
+        
+    # Cant access any of the attributes, since its static
+    @staticmethod
+    def sum(a, b):
+        return a + b
+    
+    @classmethod
+    # Is changing the values of the original class
+    def changeValues(cls):
+        cls.a = 10
+        cls.b = 10
+    
+    def divide(self):
+        return (self.a / self.b) + self.c
+
+print(Math.sum(4, 4))
+
+calc = Math(3)
+print(calc.divide())
+
+Math.changeValues()
+
+print(calc.divide())
