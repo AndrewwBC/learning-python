b1e8e1a576b0e80c54f7b2e2163c5e9c1707d41c
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-18 22:28:34 -0300

    raw string and string inside another string

--- Chapter_8/strings.py
+++ Chapter_8/strings.py
@@ -21,3 +21,31 @@ name = "Andrew"
 
 for i in range(len(name)):
     print(name[i])
+
+
+car = "Chevrolet"
+
+print(car[:4], car[4:], car[0:9:2])
+
+# string formatting
+
+oldValue = 4
+newValue = 5.25
+
+info = "Old value is %d and the new value is %.2f" % (oldValue, newValue)
+print(info)
+
+# raw and normal string
+
+print("Normal string")
+print(r"Raw string")
+
+# string inside another string
+
+str1 = "Python is a programming language"
+str2 = "Python"
+
+if str2 in str1: 
+    print("Found")
+else:
+    print("Not found")
