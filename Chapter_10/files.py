d2316422096493b0ca988e88a6348bbb363bd7fa
AndrewwBC <andrew-hot2011@hotmail.com>
2025-03-19 14:09:14 -0300

    files tell and while

--- Chapter_10/files.py
+++ Chapter_10/files.py
@@ -6,3 +6,25 @@ file.close()
 #read
 with open('file.txt', 'r') as f:
     print(f.read())
+
+
+# writelines
+
+with open('list.txt', 'w') as listTxt:
+    items = ["cup ", "mug ", "hat \n", "TV ", "radio "]
+    listTxt.writelines(items)
+
+# readline and readlines
+
+with open("list.txt", "r") as listTxt:
+    print(listTxt.readline())
+    print(listTxt.readlines())
+
+# while
+
+with open("queen.txt", "r") as queenTxt:
+    while True:
+        line = queenTxt.readline()
+        if(line == ''):
+            break
+        print(line, queenTxt.tell())
